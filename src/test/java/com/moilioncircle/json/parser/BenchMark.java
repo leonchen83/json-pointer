85266ac552e8a8654a2cf2d3d85532cc3081295e
leonchen83 <chen.bao.yi@qq.com>
2015-12-19 10:06:56 +0800

    bug fix

--- src/test/java/com/moilioncircle/json/parser/BenchMark.java
+++ src/test/java/com/moilioncircle/json/parser/BenchMark.java
@@ -3,6 +3,7 @@ package com.moilioncircle.json.parser;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.commons.io.IOUtils;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.StringReader;
@@ -36,6 +37,7 @@ public class BenchMark {
         benchmark("twitter.json", 2000); //slower than jackson
     }
 
+    //reader
     private void benchmark(String fileName, int loopSize) throws IOException, JSONParserException {
         System.out.println(fileName);
         InputStream stream = BenchMark.class.getClassLoader().getResourceAsStream(fileName);
@@ -53,7 +55,7 @@ public class BenchMark {
         //47389,36582,44350
 
         for (int i = 0; i < loopSize; i++) {
-            mapper.readTree(str);
+            mapper.readTree(streams[i]);
         }
         System.out.println("Jackson parser: " + (System.currentTimeMillis() - start) / (double) loopSize);
         for(int i=0;i<streams.length;i++){
@@ -62,7 +64,89 @@ public class BenchMark {
         start = System.currentTimeMillis();
         //73612,64926,68491
         for (int i = 0; i < loopSize; i++) {
+            ParserFactory.readTree(streams[i]);
+        }
+        System.out.println("JSON parser: " + (System.currentTimeMillis() - start) / (double) loopSize);
+    }
+
+    //str
+    private void benchmark0(String fileName, int loopSize) throws IOException, JSONParserException {
+        System.out.println(fileName);
+        InputStream stream = BenchMark.class.getClassLoader().getResourceAsStream(fileName);
+        byte[] bytes = IOUtils.toByteArray(stream);
+
+        IOUtils.closeQuietly(stream);
+        String str = new String(bytes);
+        ObjectMapper mapper = new ObjectMapper();
+
+        long start = System.currentTimeMillis();
+        //47389,36582,44350
+
+        for (int i = 0; i < loopSize; i++) {
+            mapper.readTree(str);
+        }
+        System.out.println("Jackson parser: " + (System.currentTimeMillis() - start) / (double) loopSize);
+        start = System.currentTimeMillis();
+        //73612,64926,68491
+        for (int i = 0; i < loopSize; i++) {
             ParserFactory.readTree(str);
+        }
+        System.out.println("JSON parser: " + (System.currentTimeMillis() - start) / (double) loopSize);
+    }
+
+    //inputstream
+    private void benchmark1(String fileName, int loopSize) throws IOException, JSONParserException {
+        System.out.println(fileName);
+        InputStream stream = BenchMark.class.getClassLoader().getResourceAsStream(fileName);
+        byte[] bytes = IOUtils.toByteArray(stream);
+
+        IOUtils.closeQuietly(stream);
+        String str = new String(bytes);
+        InputStream[] streams = new InputStream[loopSize];
+        for(int i=0;i<streams.length;i++){
+            streams[i] = new ByteArrayInputStream(bytes);
+        }
+        ObjectMapper mapper = new ObjectMapper();
+
+        long start = System.currentTimeMillis();
+        //47389,36582,44350
+
+        for (int i = 0; i < loopSize; i++) {
+            mapper.readTree(streams[i]);
+        }
+        System.out.println("Jackson parser: " + (System.currentTimeMillis() - start) / (double) loopSize);
+        for(int i=0;i<streams.length;i++){
+            streams[i] = new ByteArrayInputStream(bytes);
+        }
+        start = System.currentTimeMillis();
+        //73612,64926,68491
+        for (int i = 0; i < loopSize; i++) {
+            ParserFactory.readTree(streams[i]);
+        }
+        System.out.println("JSON parser: " + (System.currentTimeMillis() - start) / (double) loopSize);
+    }
+
+    //bytes
+    private void benchmark2(String fileName, int loopSize) throws IOException, JSONParserException {
+        System.out.println(fileName);
+        InputStream stream = BenchMark.class.getClassLoader().getResourceAsStream(fileName);
+        byte[] bytes = IOUtils.toByteArray(stream);
+
+        IOUtils.closeQuietly(stream);
+
+        ObjectMapper mapper = new ObjectMapper();
+
+        long start = System.currentTimeMillis();
+        //47389,36582,44350
+
+        for (int i = 0; i < loopSize; i++) {
+            mapper.readTree(bytes);
+        }
+        System.out.println("Jackson parser: " + (System.currentTimeMillis() - start) / (double) loopSize);
+        start = System.currentTimeMillis();
+        //73612,64926,68491
+        for (int i = 0; i < loopSize; i++) {
+            ParserFactory.readTree(bytes);
         }
         System.out.println("JSON parser: " + (System.currentTimeMillis() - start) / (double) loopSize);
     }
